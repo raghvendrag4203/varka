975c9ecd323fc5f7a777b3ce3ce5f12b94bef2b6
Raghvendra Gupta <raghvendrag0821@gmail.com>
2026-09-12 15:08:35 +0530

    updated thr register and otp controller

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express"
 import { validate } from "../middleware/validate.middleware.js"
 import { loginSchema, registerSchema, verifyOtpSchema } from "../schema/user.schema.js"
-import { loginController } from "../controller/user.controller.js"
+import { loginController, registerController, verifyOtpController } from "../controller/user.controller.js"
 
 const router = Router()
 
-router.post('/register', validate(registerSchema)) 
-router.post('/register/verify-otp', validate(verifyOtpSchema), ) 
+router.post('/register', validate(registerSchema), registerController) 
+router.post('/register/verify-otp', validate(verifyOtpSchema), verifyOtpController) 
 router.post('/login', validate(loginSchema), loginController)
 
 export default router 
